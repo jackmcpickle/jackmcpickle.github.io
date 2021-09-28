e8e8ae6a3290fdaaeb26c82dd5f4efeba696493e
Jack McNicol <jackmcpickle@gmail.com>
2021-09-28 22:09:36 +0930

    remove unused post css plugins

--- postcss.config.js
+++ postcss.config.js
@@ -2,15 +2,11 @@ const tailwindcss = require('tailwindcss');
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
 const postcssPresetEnv = require('postcss-preset-env');
-const postcssImport = require('postcss-import');
 const prodMode = process.env.NODE_ENV === 'production';
 
 module.exports = (ctx) => {
   const plugins = [
     postcssPresetEnv,
-    postcssImport({
-      path: ['node_modules', 'assets/css'],
-    }),
     tailwindcss,
     autoprefixer
   ];
